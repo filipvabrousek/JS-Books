1cd4065746f5d4a23203d8693e7f719fd9373018
Filip Vabrousek <fvabrousek@yahoo.com>
2016-09-05 21:33:13 +0200

    Update Show-current-time.js

--- Show-current-time.js
+++ Show-current-time.js
@@ -1,4 +1,8 @@
-//many libraries needed !!!!!!
+/*many libraries needed !!!!!!
+https://npmcdn.com/react@15.3.1/dist/react.js
+https://npmcdn.com/react-dom@15.3.1/dist/react-dom.js
+https://npmcdn.com/babel-core@5.8.38/browser.min.js
+*/
 var HelloWorld = React.createClass({
   render: function() {
     return (
