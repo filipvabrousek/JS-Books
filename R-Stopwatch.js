4e0ec01b40a781325491512683af36954addc979
Filip Vabrousek <fvabrousek@yahoo.com>
2016-09-06 22:05:30 +0200

    Update R-Stopwatch.js

--- R-Stopwatch.js
+++ R-Stopwatch.js
@@ -1,20 +1,15 @@
 var TimerExample = React.createClass({
-
   getInitialState: function() {
-
     // This is called before our render function. The object that is 
     // returned is assigned to this.state, so we can use it later.
-
     return {
       elapsed: 0
     };
   },
 
   componentDidMount: function() {
-
     // componentDidMount is called by react when the component 
     // has been rendered on the page. We can set the interval here:
-
     this.timer = setInterval(this.tick, 50);
   },
 
@@ -27,8 +22,7 @@ var TimerExample = React.createClass({
   },
 
   tick: function() {
-
-    // This function is called every 50 ms. It updates the 
+     // This function is called every 50 ms. It updates the 
     // elapsed counter. Calling setState causes the component to be re-rendered
 
     this.setState({
@@ -37,12 +31,9 @@ var TimerExample = React.createClass({
   },
 
   render: function() {
-
     var elapsed = Math.round(this.state.elapsed / 100);
-
     // This will give a number with one digit after the decimal dot (xx.x):
     var seconds = (elapsed / 10).toFixed(1);
-
     // Although we return an entire <p> element, react will smartly update
     // only the changed parts, which contain the seconds variable.
 
