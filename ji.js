b53736648c727cee76c5f36d450b9482b715b16d
Filip Vabrousek <fvabrousek@yahoo.com>
2016-09-09 16:04:32 +0200

    Create ji.js

---
+++ ji.js
@@ -0,0 +1,27 @@
+ var speech = function() {
+   var x = "lol";
+   var body = document.querySelector("body");
+   var h1 = document.querySelector("h1");
+   var recognition = new webkitSpeechRecognition();
+   recognition.continuous = true;
+   recognition.interimResults = true;
+
+   recognition.onresult = function(event) {
+     var colour = event.results[event.results.length - 1][0].transcript;
+     // make it lowercase
+     colour = colour.toLowerCase();
+     // strip the spaces out of it
+     colour = colour.replace(/\s/gi, '');
+     body.style.background = colour;
+     h1.innerHTML = colour;
+   }
+
+   recognition.start();
+
+ }
+
+ if (!('webkitSpeechRecognition' in window)) {
+   alert("Sorry you require a browser that supports speech recognition");
+ } else {
+   speech();
+ }
