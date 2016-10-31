542047edc695b92956dc64a13b4e575bd3dde892
Filip Vabrousek <fvabrousek@yahoo.com>
2016-10-31 20:24:59 +0100

    Update RNat.js

--- RNat.js
+++ RNat.js
@@ -6,7 +6,7 @@ import ReactNative, {
   View
 } from 'react-native-web';
 
-class SampleApp extends React.Component {
+class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
@@ -30,8 +30,5 @@ const styles = StyleSheet.create({
 
 
 const rootTag = document.getElementById('react-root');
-AppRegistry.registerComponent('SampleApp', () => SampleApp);
-AppRegistry.runApplication('SampleApp', { rootTag });
-
-//https://unpkg.com/react@15.3.1/dist/react.min.js
-//https://wzrd.in/bundle/react-native-web@latest
+AppRegistry.registerComponent('App', () => App);
+AppRegistry.runApplication('App', { rootTag });
