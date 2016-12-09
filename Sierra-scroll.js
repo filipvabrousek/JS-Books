1142bad5ab9c532963ab1a7799c7e034a1adf6b5
Filip Vabrousek <fvabrousek@yahoo.com>
2016-12-09 23:41:07 +0800

    Delete Sierra-scroll.js

--- Sierra-scroll.js
+++
@@ -1,9 +0,0 @@
-var img = document.querySelector("img");
-var i = 6;
-var w = 100;
-window.addEventListener("scroll", function() {
-  w -= 0.3;
-  i -= 0.3;
-  img.style.filter = "blur(" + i + "px)";
-  img.style.width = w + "%";
-});
