2d93deb4d3a41fc8c11c39c66827fe9669e72b25
Filip Vabrousek <fvabrousek@yahoo.com>
2016-12-26 17:43:55 +0100

    Update IIFE-Ello-bug.js

--- IIFE-Ello-bug.js
+++ IIFE-Ello-bug.js
@@ -1,14 +1,14 @@
- (function() {
 
-   var Ello = {
+ var Ello = (function () {
 
-       S: function(e) {
-         return document.querySelector(e);
-       },
+   var S = function(e) {
+         //return document.querySelector(e);
+       console.log(e);
+       };
 
-       /*-----------------------------------------------H FUNCTION-------------------------------------*/
-       render: function(type, props) {
-         for (var _len = arguments.length, children = Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
+
+  var render = function (type, props) {
+    for (var _len = arguments.length, children = Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
            children[_key - 2] = arguments[_key];
          };
 
@@ -18,38 +18,38 @@
            children: children
          };
 
-       },
+  };
 
-       /*-----------------------------------------------SET BOOLEAN PROP-------------------------------------*/
-       setBooleanProp: function(target, name, value) {
+    
+var setBooleanProp = function(target, name, value) {
          if (value) {
            target.setAttribute(name, value);
            target[name] = true;
          } else {
            target[name] = false;
          }
-       },
-
-       /*-----------------------------------------------REMOVE BOOLEAN PROP AND SMALL-------------------------------------*/
-       removeBooleanProp: function(target, name) {
+       };
+    
+    /*-----------------------------------------------REMOVE BOOLEAN PROP AND SMALL-------------------------------------*/
+       var removeBooleanProp = function(target, name) {
          target.removeAttribute(name);
          target[name] = false;
-       },
+       };
 
-       isEventProp: function(name) {
+       var isEventProp = function(name) {
          return /^on/.test(name);
-       },
+       };
 
-       extractEventName: function(name) {
+       var extractEventName = function(name) {
          return name.slice(2).toLowerCase();
-       },
+       };
 
-       isCustomProp: function(name) {
+       var isCustomProp = function(name) {
          return isEventProp(name) || name === 'forceUpdate';
-       },
-
-       /*-----------------------------------------------SET PROP-------------------------------------*/
-       setProp: function(target, name, value) {
+       };
+    
+     /*-----------------------------------------------SET PROP-------------------------------------*/
+       var setProp = function(target, name, value) {
          if (isCustomProp(name)) {
            return;
          } else if (name === 'className') {
@@ -59,10 +59,10 @@
          } else {
            target.setAttribute(name, value);
          }
-       },
+       };
 
        /*-----------------------------------------------REMOVE PROP-------------------------------------*/
-       removeProp: function(target, name, value) {
+       var removeProp = function(target, name, value) {
          if (isCustomProp(name)) {
            return;
          } else if (name === 'className') {
@@ -72,44 +72,48 @@
          } else {
            target.removeAttribute(name);
          }
-       },
-
-       /*-----------------------------------------------SET PROPS-------------------------------------*/
-       setProps: function(target, props) {
+       };
+  
+    
+    var setProps = function(target, props) {
          Object.keys(props).forEach(function(name) {
            setProp(target, name, props[name]);
          });
-       },
+       };
 
        /*-----------------------------------------------UPDATE PROP-------------------------------------*/
-       updateProp: function(target, name, newVal, oldVal) {
+       var updateProp = function(target, name, newVal, oldVal) {
          if (!newVal) {
            removeProp(target, name, oldVal);
          } else if (!oldVal || newVal !== oldVal) {
            setProp(target, name, newVal);
          }
-       },
+       };
 
        /*-----------------------------------------------UPDATE PROPS-------------------------------------*/
-       updateProps: function(target, newProps) {
+      var updateProps = function(target, newProps) {
          var oldProps = arguments.length <= 2 || arguments[2] === undefined ? {} : arguments[2];
          var props = Object.assign({}, newProps, oldProps);
          Object.keys(props).forEach(function(name) {
            updateProp(target, name, newProps[name], oldProps[name]);
          });
-       },
-
-       /*-----------------------------------------------ADD ELS-------------------------------------*/
-       addEventListeners: function(target, props) {
+       };
+    
+    
+    
+    
+    
+    
+    var addEventListeners = function(target, props) {
          Object.keys(props).forEach(function(name) {
            if (isEventProp(name)) {
              target.addEventListener(extractEventName(name), props[name]);
            }
          });
-       },
+       };
 
        /*-----------------------------------------------CREATE ELEMENT-------------------------------------*/
-       createElement: function(node) {
+       var createElement = function(node) {
          if (typeof node === 'string') {
            return document.createTextNode(node);
          }
@@ -118,15 +122,17 @@
          addEventListeners(el, node.props);
          node.children.map(createElement).forEach(el.appendChild.bind(el));
          return el;
-       },
+       };
 
        /*-----------------------------------------------CHANGED-------------------------------------*/
-       changed: function(node1, node2) {
+       var changed = function(node1, node2) {
          return (typeof node1 === 'undefined' ? 'undefined' : _typeof(node1)) !== (typeof node2 === 'undefined' ? 'undefined' : _typeof(node2)) || typeof node1 === 'string' && node1 !== node2 || node1.type !== node2.type || node1.props && node1.props.forceUpdate;
-       },
-
-       /*-----------------------------------------------UPDATE ELEMENT-------------------------------------*/
-       updateElement: function(parent, newNode, oldNode) {
+       };
+           
+    
+    
+    /*-----------------------------------------------UPDATE ELEMENT-------------------------------------*/
+       var updateElement = function(parent, newNode, oldNode) {
          var index = arguments.length <= 3 || arguments[3] === undefined ? 0 : arguments[3];
          if (!oldNode) {
            parent.appendChild(createElement(newNode));
@@ -149,30 +155,56 @@
            }
          }
          console.log("Element updated!");
-       }
+       };
+       
+       
+       
+       
+  /*ADD !!!!!!!!!!!!*/
+  return {
+   S:S,
+    render: render,
+     removeBooleanProp: removeBooleanProp,
+      isEventProp: isEventProp,
+      extractEventName: extractEventName,
+      isCustomProp: isCustomProp,
+      setProp: setProp,
+      removeProp: removeProp,
+      setProps: setProps,
+      updateProp: updateProp,
+      updateProps: updateProps,
+      addEventListeners: addEventListeners,
+      createElement: createElement,
+      changed: changed,
+      updateElement: updateElement
+  };
 
-       /*-----------------------------------------------CUSTOM-------------------------------------*/
+})();
 
-     } //end of Ello object
 
-   /*                      EXPOSE FUNCTIONS TO OUTER SCOPE           */
-   window.Ello = Ello;
- })();
 
- /*-----------------------------------------------CUSTOM-------------------------------------*/
 
- var el = Ello.render('h2', {
-     className: 'title',
-     onClick: function onClick() {
-       this.style.color = "green";
-     }
-   },
-   'Hello world! Click to change color.');
+/*-----------------------------------------------CUSTOM-------------------------------------*/
 
- var root = Ello.S('#container');
- var reload = Ello.S('#reload');
- Ello.updateElement(root, el);
+    Ello.S("hihi");
+    
+const el = Ello.render('h2', {
+    className: 'title',
+    onClick: function onClick() {
+      this.style.color = "green";
+    }
+  },
+  'Hello world! Click to change color.');
 
- reload.addEventListener('click', function() {
-   updateElement(S('#container'), el);
- });
+
+var root = Ello.S('#container');
+var reload = Ello.S('#reload');
+
+Ello.updateElement(root, el);
+
+    
+reload.addEventListener('click', function() {
+  Ello.updateElement(root, el);
+});
+
+        
