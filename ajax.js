202136276d700b65f3efc3a3ea375bd1c60575a7
Filip Vabrousek <fvabrousek@yahoo.com>
2017-04-30 14:18:56 +0200

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -1,12 +1,12 @@
 function showHint(str) {
     if (str.length == 0) { 
-        document.getElementById("txtHint").innerHTML = "";
+        document.querySelector("#hint").innerHTML = "";
         return;
     } else {
         const xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("hint").innerHTML = this.responseText;
+                document.querySelector("#hint").innerHTML = this.responseText;
             }
         };
         xmlhttp.open("GET", `gethint.php?q=${str}`, true);
