67ee2e1473ca776bb4ec898098d13a718f5f6a84
Filip Vabrousek <fvabrousek@yahoo.com>
2017-04-30 14:22:35 +0200

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -2,34 +2,7 @@
 // Array with names
 $a[] = "Anna";
 $a[] = "Brittany";
-$a[] = "Cinderella";
-$a[] = "Diana";
-$a[] = "Eva";
-$a[] = "Fiona";
-$a[] = "Gunda";
-$a[] = "Hege";
-$a[] = "Inga";
-$a[] = "Johanna";
-$a[] = "Kitty";
-$a[] = "Linda";
-$a[] = "Nina";
-$a[] = "Ophelia";
-$a[] = "Petunia";
-$a[] = "Amanda";
-$a[] = "Raquel";
-$a[] = "Cindy";
-$a[] = "Doris";
-$a[] = "Eve";
-$a[] = "Evita";
-$a[] = "Sunniva";
-$a[] = "Tove";
-$a[] = "Unni";
-$a[] = "Violet";
-$a[] = "Liza";
-$a[] = "Elizabeth";
-$a[] = "Ellen";
-$a[] = "Wenche";
-$a[] = "Vicky";
+....
 
 // get the q parameter from URL
 $q = $_REQUEST["q"];
