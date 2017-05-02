0f47bf588a7b944239c595624e083cc70bc49929
Filip Vabrousek <fvabrousek@yahoo.com>
2017-05-02 15:44:14 +0200

    Update w.js

--- w.js
+++ w.js
@@ -1,7 +1,98 @@
-class Hello extends React.Component {
-  render() {
-    return <h1>Hello world</h1>;
+class Search extends React.component{
+  getInitialState() {
+    return {
+      searchString: ''
+    };
   }
-}
+    
+    
+    
+    
+  handleChange(e) {
+    this.setState({
+      searchString: e.target.value
+    });
+  }
+    
+    
+    
+  render() {
+    var libraries = this.props.items,
+      searchString = this.state.searchString.trim().toLowerCase();
+    if (searchString.length > 0) {
+      // We are searching. Filter the results.
+      libraries = libraries.filter(function(l) {
+        return l.name.toLowerCase().match(
+          searchString);
+      });
+    }
+    
+    return 
+      (<div > < input type = "text"
+        value = {
+            this.state.searchString
+        }
+        onChange = {
+            this.handleChange
+        }
+        placeholder = "Type here" / > < ul > {
+            libraries.map(function(l) {
+                return(
+              <li > {
+                    l.name } < a href = { l.url} > {l.url} < /a></li >
+            } </ul>
 
-ReactDOM.render(<Hello />, document.querySelector("#ex"));
+         }         </div >;
+  });
+};
+                  
+                  
+                  
+                  
+var libraries = [{
+  name: 'Backbone.js',
+  url: 'http://documentcloud.github.io/backbone/'
+}, {
+  name: 'AngularJS',
+  url: 'https://angularjs.org/'
+}, {
+  name: 'jQuery',
+  url: 'http://jquery.com/'
+}, {
+  name: 'Prototype',
+  url: 'http://www.prototypejs.org/'
+}, {
+  name: 'React',
+  url: 'http://facebook.github.io/react/'
+}, {
+  name: 'Ember',
+  url: 'http://emberjs.com/'
+}, {
+  name: 'Knockout.js',
+  url: 'http://knockoutjs.com/'
+}, {
+  name: 'Dojo',
+  url: 'http://dojotoolkit.org/'
+}, {
+  name: 'Mootools',
+  url: 'http://mootools.net/'
+}, {
+  name: 'Underscore',
+  url: 'http://documentcloud.github.io/underscore/'
+}, {
+  name: 'Lodash',
+  url: 'http://lodash.com/'
+}, {
+  name: 'Moment',
+  url: 'http://momentjs.com/'
+}, {
+  name: 'Express',
+  url: 'http://expressjs.com/'
+}, {
+  name: 'Koa',
+  url: 'http://koajs.com/'
+}, ];
+
+ReactDOM.render( < SearchExample items = {
+            libraries}, document.getElementById('container;
+'));
