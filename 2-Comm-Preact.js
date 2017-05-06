0c072e042c613e992a7be15d98e15dfef8ac7436
Filip Vabrousek <fvabrousek@yahoo.com>
2017-05-06 17:45:34 +0200

    Update 2-Comm-Preact.js

--- 2-Comm-Preact.js
+++ 2-Comm-Preact.js
@@ -674,9 +674,10 @@
 !((t, e) => {"object"==typeof exports&&"undefined"!=typeof module?module.exports=e(require("preact")):"function"==typeof define&&define.amd?define(["preact"],e):t.linkState=e(t.preact)})(this,t => {function e(t,e,n,o){for(o=0,e=e.split?e.split("."):e;t&&o<e.length;)t=t[e[o++]];return void 0===t?n:t}function n(t,n,o){const r=n.split(".");return function(n){for(var i=n&&n.target||this,f={},p=f,u="string"==typeof o?e(n,o):i.nodeName?i.type.match(/^che|rad/)?i.checked:i.value:n,a=0;a<r.length-1;a++)p=p[r[a]]||(p[r[a]]=!a&&t.state[r[a]]||{});p[r[a]]=u,t.setState(f)}}return t.Component.prototype.linkState=function(t,e){return n(this,t,e)},n});
 
 /*
-let { h, render, Component } = preact; // import { ... } from 'preact';
-
-
+let { h, render, Component } = preact; 
+// import { ... } from 'preact';
+/** @jsx h */
+/*
 class App extends Component {
   state = {
     text: "Hello!"
@@ -706,6 +707,7 @@ class Result extends Component {
 }
 
 render(<App />, document.body);
+
 
 
 
